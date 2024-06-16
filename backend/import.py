5b10b4420660a91babb494a03ee9fd8ab374da54
AbnerTeng <abnerteng16@gmail.com>
2024-06-16 23:42:31 +0800

    [Add] Backend CRUD

---
+++ backend/import.py
@@ -0,0 +1,234 @@
+import pandas as pd
+import sqlite3
+import os
+db_file = 'bikestore.db'
+
+
+if os.path.exists(db_file):
+    os.remove(db_file)
+    print(f"Deleted existing database file: {db_file}")
+else:
+    print(f"No existing database file found: {db_file}")
+
+database = sqlite3.connect('bikestore.db')
+cursor = database.cursor()
+
+
+
+csv = 'C:/Users/ANDY/Downloads/archive/categories.csv'
+data_flow = pd.read_csv(csv)
+
+cursor.execute('''
+DROP TABLE IF EXISTS categories
+''')
+cursor.execute('''
+CREATE TABLE categories(
+    category_id TEXT PRIMARY KEY,
+    category_name TEXT
+)
+''')
+database.commit()
+data_flow.to_sql('categories', database, if_exists='replace', index=False)
+print("成功存入categories")
+
+#///////////////////////////////////////////////////////////////////////
+
+
+csv = 'C:/Users/ANDY/Downloads/archive/brands.csv'
+data_flow = pd.read_csv(csv)
+
+cursor.execute('''
+DROP TABLE IF EXISTS brands
+''')
+cursor.execute('''
+CREATE TABLE brands(
+    brand_id TEXT,
+    brand_name TEXT
+)
+''')
+database.commit()
+data_flow.to_sql('brands', database, if_exists='replace', index=False)
+print("成功存入brands")
+
+#///////////////////////////////////////////////////////////////////////
+
+
+csv = 'C:/Users/ANDY/Downloads/archive/products.csv'
+data_flow = pd.read_csv(csv)
+
+cursor.execute('''
+DROP TABLE IF EXISTS products
+''')
+cursor.execute('''
+CREATE TABLE products(
+    product_id TEXT PRIMARY KEY,
+    product_name TEXT,
+    brand_id TEXT,
+    category_id TEXT,
+    model_year TEXT,
+    list_price REAL,
+    FOREIGN KEY (category_id) REFERENCES categories(category_id),
+    FOREIGN KEY (brand_id) REFERENCES brands(brand_id)
+)
+''')
+database.commit()
+data_flow.to_sql('products', database, if_exists='replace', index=False)
+print("成功存入products")
+
+#///////////////////////////////////////////////////////////////////////
+
+
+csv = 'C:/Users/ANDY/Downloads/archive/stocks.csv'
+data_flow = pd.read_csv(csv)
+
+cursor.execute('''
+DROP TABLE IF EXISTS stocks
+''')
+cursor.execute('''
+CREATE TABLE stocks(
+    store_id TEXT,
+    product_id TEXT,
+    quantity TEXT,
+    PRIMARY KEY(store_id,product_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
+)
+''')
+database.commit()
+data_flow.to_sql('stocks', database, if_exists='replace', index=False)
+print("成功存入stocks")
+cursor.execute("SELECT * from stocks")
+#///////////////////////////////////////////////////////////////////////
+
+
+csv = 'C:/Users/ANDY/Downloads/archive/order_items.csv'
+data_flow = pd.read_csv(csv)
+
+cursor.execute('''
+DROP TABLE IF EXISTS order_items
+''')
+cursor.execute('''
+CREATE TABLE order_items(
+    order_id TEXT,
+    item_id TEXT,
+    product_id TEXT,
+    quantity TEXT,
+    list_price TEXT,
+    discount TEXT,
+    PRIMARY KEY (order_id, item_id)    
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
+)
+''')
+database.commit()
+data_flow.to_sql('order_items', database, if_exists='replace', index=False)
+print("成功存入order_items")
+#///////////////////////////////////////////////////////////////////////
+
+
+csv = 'C:/Users/ANDY/Downloads/archive/stores.csv'
+data_flow = pd.read_csv(csv)
+
+cursor.execute('''
+DROP TABLE IF EXISTS stores
+''')
+cursor.execute('''
+CREATE TABLE stores (
+    store_id TEXT PRIMARY KEY,
+    store_name TEXT,
+    phone TEXT,
+    email TEXT,
+    street TEXT,
+    city TEXT,
+    state TEXT,
+    zip_code TEXT,
+    FOREIGN KEY (store_id) REFERENCES stocks(store_id)
+)
+''')
+database.commit()
+data_flow.to_sql('stores', database, if_exists='replace', index=False)
+print("成功存入stores")
+#///////////////////////////////////////////////////////////////////////
+
+
+
+csv = 'C:/Users/ANDY/Downloads/archive/staffs.csv'
+data_flow = pd.read_csv(csv)
+
+cursor.execute('''
+DROP TABLE IF EXISTS staffs
+''')
+cursor.execute('''
+CREATE TABLE staffs (
+    staff_id TEXT PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name  TEXT NOT NULL,       
+    email TEXT,
+    phone TEXT,
+    active TEXT,
+    store_id TEXT,
+    manager_id TEXT,
+    FOREIGN KEY (store_id) REFERENCES stores(store_id),
+    FOREIGN KEY (manager_id) REFERENCES staffs(staff_id)
+)
+''')
+database.commit()
+data_flow.to_sql('staffs', database, if_exists='replace', index=False)
+print("成功存入staffs")
+
+# #///////////////////////////////////////////////////////////////////////
+
+
+csv = 'C:/Users/ANDY/Downloads/archive/customers.csv'
+data_flow = pd.read_csv(csv)
+
+cursor.execute('''
+DROP TABLE IF EXISTS customers
+''')
+cursor.execute('''
+CREATE TABLE customers (
+    customer_id TEXT PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name  TEXT NOT NULL,       
+    phone TEXT,
+    email TEXT,
+    street TEXT,
+    city TEXT,
+    state TEXT,
+    zip_code TEXT
+)
+''')
+database.commit()
+data_flow.to_sql('customers', database, if_exists='replace', index=False)
+print("成功存入customer")
+#///////////////////////////////////////////////////////////////////////
+
+
+csv = 'C:/Users/ANDY/Downloads/archive/orders.csv'
+data_flow = pd.read_csv(csv)
+
+cursor.execute('''
+DROP TABLE IF EXISTS orders
+''')
+cursor.execute('''
+CREATE TABLE orders (
+    order_id TEXT PRIMARY KEY,
+    customer_id TEXT,
+    order_status TEXT,
+    order_date TEXT
+    required_date TEXT    
+    shipped_date TEXT,
+    store_id TEXT,
+    staff_id TEXT,
+    FOREIGN KEY (staff_id) REFERENCES staffs(staff_id),
+    FOREIGN KEY (store_id) REFERENCES stores(store_id),
+    FOREIGN KEY (order_id) REFERENCES order_items(order_id),
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+)
+''')
+
+database.commit()
+data_flow.to_sql('orders', database, if_exists='replace', index=False)
+print("成功存入orders")
+
+
+database.close()
+
---
+++ backend/update.py
@@ -0,0 +1,188 @@
+import os
+from flask import Flask, request, render_template, g
+import sqlite3
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+
+# 根據提供的目錄結構設置模板和靜態文件夾
+static_folder = os.path.join(current_dir, '../frontend/static')
+template_folder = os.path.join(current_dir, '../frontend/templates')
+
+# 打印路径用于调试
+print(f"Static folder: {static_folder}")
+print(f"Template folder: {template_folder}")
+
+app = Flask(__name__, static_folder=static_folder, template_folder=template_folder)
+
+# 全局變量
+id = 1616
+database = os.path.join(current_dir, 'bikestore.db')
+
+def get_db_connection():
+    conn = sqlite3.connect(database)
+    return conn
+
+@app.before_request
+def before_request():
+    g.db = get_db_connection()
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+@app.route("/")
+def show1():
+    return render_template('index.html')
+
+@app.route("/new")
+def show2():
+    return render_template('new.html')
+
+@app.route("/new_tackle", methods=["POST"])
+def new():
+    global id
+    
+    customer_id = request.form['customer_id']
+    order_status = request.form['order_status']
+    order_date = request.form['order_date']
+    required_date = request.form['required_date']
+    shipped_date = request.form['shipped_date']
+    store_id = request.form['store_id']
+    staff_id = request.form['staff_id']
+    item_id = 1
+    products_id = request.form['product_id']
+    quantities = request.form['quantity'] 
+    discount = request.form['discount']
+    
+    cursor = g.db.cursor()
+    cursor.execute("SELECT customer_id FROM customers WHERE customer_id = ?;", (customer_id,))
+    customer_search = cursor.fetchone()
+    if customer_search == None:
+        result = "您的帳號不存在，請先創建帳戶再進行訂購"
+        return render_template('result.html', result=result)
+    
+    product_id = products_id.split(',')
+    quantity = quantities.split(',')
+    arr = []
+    cost = 0
+    
+    for i in range(len(product_id)):
+        cursor.execute("SELECT product_id, quantity FROM stocks WHERE product_id = ? AND store_id = ?;", (product_id[i], store_id))
+        stock_quantity = int(cursor.fetchone()[1])
+        if stock_quantity >= int(quantity[i]):
+            arr.append(product_id[i])
+            remain_quantity = stock_quantity - int(quantity[i])
+            cursor.execute("UPDATE stocks SET quantity = ? WHERE product_id = ? AND store_id = ?;", (str(remain_quantity), product_id[i], store_id))
+            cursor.execute("SELECT list_price FROM products WHERE product_id = ?;", (product_id[i],))
+            list_price = float(cursor.fetchone()[0])
+            cost += int(quantity[i]) * list_price
+            cursor.execute(
+                "INSERT INTO order_items (order_id, item_id, product_id, quantity, list_price, discount) VALUES (?, ?, ?, ?, ?, ?);", 
+                (str(id), str(item_id), str(product_id[i]), quantity[i], list_price, discount)
+            )
+            item_id += 1
+    
+    if len(arr) > 0:
+        cursor.execute(
+            "INSERT INTO orders (order_id, customer_id, order_status, order_date, required_date, shipped_date, store_id, staff_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?);", 
+            (id, customer_id, order_status, order_date, required_date, shipped_date, store_id, staff_id)
+        )
+        id += 1
+        g.db.commit()
+        cost = cost * float(discount)
+        result = f"您購物車裡id = {arr} 商品新增成功, 總共{cost}元"
+    else:
+        result = '存貨不足，您的訂單新增失敗！'
+    
+    return render_template('result.html', result=result)
+
+@app.route("/delete")
+def show3():
+    return render_template('delete.html')
+
+@app.route("/delete_tackle", methods=["POST"])
+def delete():
+    delete_id = request.form['order_id']
+    cursor = g.db.cursor()
+    cursor.execute("SELECT order_id FROM orders WHERE order_id = ?;", (delete_id,))
+    data = cursor.fetchone()
+    result = '找不到您的訂單，無法進行刪除'
+    if data is None:
+        return render_template('result.html', result=result)
+    else:
+        cursor.execute("SELECT product_id, quantity FROM order_items WHERE order_id = ?;", (delete_id,))
+        order_quantity = cursor.fetchall()
+        for product, quantity in order_quantity:
+            cursor.execute("SELECT quantity FROM stocks WHERE product_id = ?;", (product,))
+            remain_quantity = cursor.fetchone()[0]
+            cursor.execute("UPDATE stocks SET quantity = ? WHERE product_id = ?;", (remain_quantity + quantity, product))
+        cursor.execute("DELETE FROM orders WHERE order_id = ?;", (delete_id,))
+        cursor.execute("DELETE FROM order_items WHERE order_id = ?;", (delete_id,))
+        result = "您的訂單已刪除完成"
+        return render_template("result.html", result=result)
+
+# 新增更新訂單的路由
+@app.route("/update")
+def show4():
+    return render_template('update.html')
+
+@app.route("/update_tackle", methods=["POST"])
+def update():
+    order_id = request.form['order_id']
+    customer_id = request.form['customer_id']
+    order_status = request.form['order_status']
+    order_date = request.form['order_date']
+    required_date = request.form['required_date']
+    shipped_date = request.form['shipped_date']
+    store_id = request.form['store_id']
+    staff_id = request.form['staff_id']
+    
+    cursor = g.db.cursor()
+    cursor.execute("SELECT order_id FROM orders WHERE order_id = ?;", (order_id,))
+    order_search = cursor.fetchone()
+    if order_search is None:
+        result = "找不到您的訂單，無法進行更新"
+        return render_template('result.html', result=result)
+
+    cursor.execute(
+        "UPDATE orders SET customer_id = ?, order_status = ?, order_date = ?, required_date = ?, shipped_date = ?, store_id = ?, staff_id = ? WHERE order_id = ?;",
+        (customer_id, order_status, order_date, required_date, shipped_date, store_id, staff_id, order_id)
+    )
+    g.db.commit()
+    result = f"訂單 {order_id} 已成功更新"
+    
+    return render_template('result.html', result=result)
+
+@app.route("/update_order_item")
+def show_update_order_item():
+    return render_template('update_order_item.html')
+
+@app.route("/update_order_item_tackle", methods=["POST"])
+def update_order_item():
+    order_id = request.form['order_id']
+    item_id = request.form['item_id']
+    product_id = request.form['product_id']
+    quantity = request.form['quantity']
+    list_price = request.form['list_price']
+    discount = request.form['discount']
+
+    cursor = g.db.cursor()
+    cursor.execute("SELECT order_id, item_id FROM order_items WHERE order_id = ? AND item_id = ?;", (order_id, item_id))
+    order_item_search = cursor.fetchone()
+    if order_item_search is None:
+        result = "找不到您的訂單項目，無法進行更新"
+        return render_template('result.html', result=result)
+
+    cursor.execute(
+        "UPDATE order_items SET product_id = ?, quantity = ?, list_price = ?, discount = ? WHERE order_id = ? AND item_id = ?;",
+        (product_id, quantity, list_price, discount, order_id, item_id)
+    )
+    g.db.commit()
+    result = f"訂單項目 {order_id}-{item_id} 已成功更新"
+    
+    return render_template('result.html', result=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
